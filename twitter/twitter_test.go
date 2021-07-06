7bef69b4f8848ae1c5e388cd4abe51a0778bc4a2
jubobs <jubobs-oss@protonmail.com>
2021-07-06 15:35:25 +0200

    Improve code coverage

--- twitter/twitter_test.go
+++ twitter/twitter_test.go
@@ -14,3 +14,39 @@ func TestUsernameTooLong(t *testing.T) {
 		t.Errorf("twitter.IsValid(%q): got %t; want %t", username, got, want)
 	}
 }
+
+func TestUsernameTooShort(t *testing.T) {
+	username := "foo"
+	want := false
+	got := twitter.IsValid(username)
+	if got != want {
+		t.Errorf("twitter.IsValid(%q): got %t; want %t", username, got, want)
+	}
+}
+
+func TestUsernameContainsTwitter(t *testing.T) {
+	username := "jub0bsOnTwitter"
+	want := false
+	got := twitter.IsValid(username)
+	if got != want {
+		t.Errorf("twitter.IsValid(%q): got %t; want %t", username, got, want)
+	}
+}
+
+func TestUsernameContainsIllegalChars(t *testing.T) {
+	username := "-jub-0bs-"
+	want := false
+	got := twitter.IsValid(username)
+	if got != want {
+		t.Errorf("twitter.IsValid(%q): got %t; want %t", username, got, want)
+	}
+}
+
+func TestUsernameIsValid(t *testing.T) {
+	username := "jub0bs"
+	want := true
+	got := twitter.IsValid(username)
+	if got != want {
+		t.Errorf("twitter.IsValid(%q): got %t; want %t", username, got, want)
+	}
+}
