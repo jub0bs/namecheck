528c1304f790571fbfd867260704ff72dabbda2c
jubobs <jubobs-oss@protonmail.com>
2021-07-07 10:21:03 +0200

    Get usernames to test from command-line args

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,13 +2,26 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/jub0bs/namecheck/github"
 	"github.com/jub0bs/namecheck/twitter"
 )
 
 func main() {
-	username := "jub0bs"
-	fmt.Println(twitter.IsValid(username))
-	fmt.Println(github.IsValid(username))
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "usage: namecheck <username>...")
+		os.Exit(1)
+	}
+	var valid []string
+	var invalid []string
+	for _, username := range os.Args[1:] {
+		if twitter.IsValid(username) && github.IsValid(username) {
+			valid = append(valid, username)
+		} else {
+			invalid = append(invalid, username)
+		}
+	}
+	fmt.Println("usernames valid on both Twitter and GitHub:", valid)
+	fmt.Println("usernames invalid on either Twitter or GitHub:", invalid)
 }
