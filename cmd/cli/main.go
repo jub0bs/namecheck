5ae22f4b0620feef847ba8737ce595e955bce2b0
jubobs <jubobs-oss@protonmail.com>
2021-07-08 14:42:59 +0200

    Use channel to aggregate results in main

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"sync"
 
 	"github.com/jub0bs/namecheck"
 	"github.com/jub0bs/namecheck/github"
 	"github.com/jub0bs/namecheck/twitter"
 )
+
+type Result struct {
+	Username  string
+	Platform  string
+	Valid     bool
+	Available bool
+	Err       error
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -24,25 +33,44 @@ func main() {
 	}
 	// let's pretend that we support many platforms
 	var checkers []namecheck.Checker
-	for i := 0; i < 50; i++ {
+	for i := 0; i < 2; i++ {
 		checkers = append(checkers, &tw, &gh)
 	}
+
+	ch := make(chan Result)
+	var wg sync.WaitGroup
+	wg.Add(len(checkers))
 	for _, checker := range checkers {
-		valid := checker.IsValid(username)
-		var (
-			avail bool
-			err   error
-		)
-		if valid {
-			avail, err = checker.IsAvailable(username)
-		}
-		fmt.Printf(
-			"[%q on %s] valid: %t; available: %t; error: %v\n",
-			username,
-			checker.String(),
-			valid,
-			avail,
-			err,
-		)
+		go check(checker, username, &wg, ch)
 	}
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	results := make([]Result, 0, len(checkers))
+	for res := range ch {
+		results = append(results, res)
+	}
+
+	fmt.Println(results)
+}
+
+func check(
+	checker namecheck.Checker,
+	username string,
+	wg *sync.WaitGroup,
+	ch chan Result,
+) {
+	defer wg.Done()
+	res := Result{
+		Username: username,
+		Platform: checker.String(),
+	}
+	res.Valid = checker.IsValid(username)
+	if res.Valid {
+		res.Available, res.Err = checker.IsAvailable(username)
+	}
+	ch <- res
 }
