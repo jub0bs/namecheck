cec3696546fee86ab1041e11f41e1530282765a9
jub0bs <jcretel-infosec+github@protonmail.com>
2024-06-06 14:27:49 +0200

    declare IsValid function

--- main.go
+++ main.go
@@ -8,13 +8,20 @@ import (
 
 func main() {
 	username := "jub0bs"
-	if strings.HasPrefix(username, "-") ||
-		strings.HasSuffix(username, "-") ||
-		strings.Contains(username, "--") {
-		return
-	}
-	if valid, _ := regexp.MatchString("^[a-zA-Z0-9-]{3,39}$", username); !valid {
+	if !IsValid(username) {
 		return
 	}
 	fmt.Println(username)
+}
+
+func IsValid(username string) bool {
+	if strings.HasPrefix(username, "-") ||
+		strings.HasSuffix(username, "-") ||
+		strings.Contains(username, "--") {
+		return false
+	}
+	if valid, _ := regexp.MatchString("^[a-zA-Z0-9-]{3,39}$", username); !valid {
+		return false
+	}
+	return true
 }
