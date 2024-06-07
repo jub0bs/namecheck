8ee73fa01f59b6cf0f67cc6d2b2df66660cf5a87
jub0bs <jcretel-infosec+github@protonmail.com>
2024-06-07 09:25:09 +0200

    exercise github and reddit package in main

--- main.go
+++ main.go
@@ -2,12 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/jub0bs/namecheck/github"
+	"github.com/jub0bs/namecheck/reddit"
 )
 
 func main() {
-	fmt.Println(github.IsValid("jub0bs"))
-	fmt.Println(github.IsValid("jub  0bs"))
-	fmt.Println(github.IsValid("jub___0bs"))
+	const username = "jub0bs"
+	if !github.IsValid(username) {
+		fmt.Printf("%q is not valid on GitHub\n", username)
+	} else {
+		fmt.Printf("%q is valid on GitHub\n", username)
+		avail, err := github.IsAvailable(username)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unknown availibility of %q on GitHub", username)
+			return
+		}
+		if !avail {
+			fmt.Printf("%q is not available on GitHub\n", username)
+		} else {
+			fmt.Printf("%q is available on GitHub\n", username)
+		}
+	}
+
+	if !reddit.IsValid(username) {
+		fmt.Printf("%q is not valid on Reddit\n", username)
+	} else {
+		fmt.Printf("%q is valid on Reddit\n", username)
+		avail, err := reddit.IsAvailable(username)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "unknown availibility of %q on Reddit", username)
+			return
+		}
+		if !avail {
+			fmt.Printf("%q is not available on Reddit\n", username)
+		} else {
+			fmt.Printf("%q is available on Reddit\n", username)
+		}
+	}
 }
