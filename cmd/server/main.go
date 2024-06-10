a6a149b6f65cb27d287f5be0aa3d4eb540365660
jub0bs <jcretel-infosec+github@protonmail.com>
2024-06-10 10:48:26 +0200

    experiment with concurrency in server subfolder

---
+++ cmd/server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func grindCoffeeBeans() {
+	fmt.Println("Grinding coffee beans...")
+}
+
+func frothMilk() {
+	fmt.Println("Frothing milk...")
+}
+
+func main() {
+	go grindCoffeeBeans()
+	go frothMilk()
+}
