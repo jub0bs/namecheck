25abfa87bc64e6ce6b0a79123c3d0fd6cd3db1b3
jub0bs <jcretel-infosec+github@protonmail.com>
2024-06-10 15:27:26 +0200

    turn command-line tool into a server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,91 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
 
-func grindCoffeeBeans() {
-	fmt.Println("Grinding coffee beans...")
+	"github.com/jub0bs/namecheck/github"
+	"github.com/jub0bs/namecheck/reddit"
+)
+
+type Validator interface {
+	IsValid(string) bool
 }
 
-func frothMilk() {
-	fmt.Println("Frothing milk...")
+type Availabler interface {
+	IsAvailable(string) (bool, error)
+}
+
+type Checker interface {
+	Validator
+	Availabler
+	fmt.Stringer
+}
+
+type Result struct {
+	Username  string
+	Platform  string
+	Valid     bool
+	Available bool
+	Err       error
 }
 
 func main() {
-	go grindCoffeeBeans()
-	go frothMilk()
+	http.HandleFunc("GET /check", handleCheck)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleCheck(w http.ResponseWriter, req *http.Request) {
+	username := req.URL.Query().Get("username")
+	if username == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	gh := github.GitHub{
+		Client: http.DefaultClient,
+	}
+	re := reddit.Reddit{
+		Client: http.DefaultClient,
+	}
+	var checkers []Checker
+	for range 20 {
+		checkers = append(checkers, &gh, &re)
+	}
+	resultCh := make(chan Result)
+	var wg sync.WaitGroup
+	for _, checker := range checkers {
+		wg.Add(1)
+		go check(checker, username, &wg, resultCh)
+	}
+	go func() {
+		wg.Wait()
+		close(resultCh)
+	}()
+	results := make([]Result, 0, len(checkers))
+	for res := range resultCh {
+		results = append(results, res)
+	}
+	fmt.Fprint(w, results)
+}
+
+func check(
+	checker Checker,
+	username string,
+	wg *sync.WaitGroup,
+	resultCh chan<- Result,
+) {
+	defer wg.Done()
+	res := Result{
+		Username: username,
+		Platform: checker.String(),
+		Valid:    checker.IsValid(username),
+	}
+	if !res.Valid {
+		resultCh <- res
+		return
+	}
+	res.Available, res.Err = checker.IsAvailable(username)
+	resultCh <- res
 }
