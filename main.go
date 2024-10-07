4442882d9cba300ee3dc1826f5fdc42714451cca
jub0bs <jcretel-infosec+github@protonmail.com>
2024-10-07 12:24:55 +0200

    implement validation on GitHub

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, 世界")
+	const username = "jub0bs"
+	if strings.HasPrefix(username, "-") ||
+		strings.HasSuffix(username, "-") ||
+		strings.Contains(username, "--") {
+		return
+	}
+	if ok, _ := regexp.MatchString("^[a-zA-Z0-9-]{3,39}$", username); !ok {
+		return
+	}
+	fmt.Println(username)
 }
