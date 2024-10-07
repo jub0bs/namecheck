282cf7cd8934d4d624c6bab52a7dd31f4b85b72d
jub0bs <jcretel-infosec+github@protonmail.com>
2024-10-07 16:47:59 +0200

    create github package

--- main.go
+++ main.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"fmt"
-	"regexp"
-	"strings"
+
+	"github.com/jub0bs/namecheck/github"
 )
 
-var re = regexp.MustCompile("^[a-zA-Z0-9-]{3,39}$")
-
 func main() {
-	fmt.Println(IsValid("jub0bs")) // true
-	fmt.Println(IsValid("-_-"))    // false
-}
-
-func IsValid(username string) bool {
-	return !strings.HasPrefix(username, "-") &&
-		!strings.HasSuffix(username, "-") &&
-		!strings.Contains(username, "--") &&
-		re.MatchString(username)
+	fmt.Println(github.IsValid("jub0bs")) // true
+	fmt.Println(github.IsValid("-_-"))    // false
 }
