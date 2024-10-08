9c9bab1efab8fa9915cc8d00ea64c46816a9c7df
jub0bs <jcretel-infosec+github@protonmail.com>
2024-10-08 06:41:53 +0200

    bring code coverage to 100%

--- github/github_test.go
+++ github/github_test.go
@@ -1,6 +1,7 @@
 package github_test
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/jub0bs/namecheck/github"
@@ -9,6 +10,42 @@ import (
 func TestUsernameContainsTwoConsecutiveHyphens(t *testing.T) {
 	const username = "jub0bs--on-GitHub"
 	const want = false
+	got := github.IsValid(username)
+	if got != want {
+		t.Errorf("github.IsValid(%q): got %t; want %t", username, got, want)
+	}
+}
+
+func TestUsernameTooShort(t *testing.T) {
+	const username = "ab"
+	const want = false
+	got := github.IsValid(username)
+	if got != want {
+		t.Errorf("github.IsValid(%q): got %t; want %t", username, got, want)
+	}
+}
+
+func TestUsernameTooLong(t *testing.T) {
+	username := strings.Repeat("a", 40)
+	const want = false
+	got := github.IsValid(username)
+	if got != want {
+		t.Errorf("github.IsValid(%q): got %t; want %t", username, got, want)
+	}
+}
+
+func TestUsernameInvalidChars(t *testing.T) {
+	const username = "^_^'"
+	const want = false
+	got := github.IsValid(username)
+	if got != want {
+		t.Errorf("github.IsValid(%q): got %t; want %t", username, got, want)
+	}
+}
+
+func TestUsernameValid(t *testing.T) {
+	const username = "jub0bs"
+	const want = true
 	got := github.IsValid(username)
 	if got != want {
 		t.Errorf("github.IsValid(%q): got %t; want %t", username, got, want)
