8cbf046c9abcda203208e67d7cec137fad5bfcd9
jub0bs <jcretel-infosec+github@protonmail.com>
2024-10-08 10:12:07 +0200

    implement availability check on GitHub

--- github/github.go
+++ github/github.go
@@ -1,6 +1,9 @@
 package github
 
 import (
+	"errors"
+	"net/http"
+	"net/url"
 	"regexp"
 	"strings"
 )
@@ -12,4 +15,24 @@ func IsValid(username string) bool {
 		!strings.HasSuffix(username, "-") &&
 		!strings.Contains(username, "--") &&
 		re.MatchString(username)
+}
+
+func IsAvailable(username string) (bool, error) {
+	addr, err := url.JoinPath("https://github.com", url.PathEscape(username))
+	if err != nil {
+		return false, err
+	}
+	resp, err := http.Get(addr)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+	switch resp.StatusCode {
+	case http.StatusNotFound:
+		return true, nil
+	case http.StatusOK:
+		return false, nil
+	default:
+		return false, errors.New("unknown availability")
+	}
 }
