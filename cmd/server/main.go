605159c1409da17e190fca642cc9ff7cfb4bdf21
jub0bs <jcretel-infosec+github@protonmail.com>
2024-10-09 15:38:58 +0200

    add Hello World server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("GET /hello", handleHello)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleHello(w http.ResponseWriter, _ *http.Request) {
+	io.WriteString(w, "Hello, world!")
+}
