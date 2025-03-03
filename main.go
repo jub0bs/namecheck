3033610b6a7276faa2d0db240e447d8df9d35ad7
jub0bs <jcretel-infosec+github@protonmail.com>
2025-03-03 14:03:25 +0100

    extract validation logic to function

--- main.go
+++ main.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"regexp"
 	"strings"
 )
 
+var re = regexp.MustCompile("^[A-Za-z0-9-]{3,39}$")
+
 func main() {
 	username := "jub0bs-"
-	re, err := regexp.Compile("^[A-Za-z0-9-]{3,39}$")
-	if err != nil {
-		log.Fatal(err)
-	}
-	if strings.HasPrefix(username, "-") ||
-		strings.HasSuffix(username, "-") ||
-		strings.Contains(username, "--") ||
-		!re.MatchString(username) {
+	if !IsValid(username) {
 		return
 	}
 	fmt.Println(username)
+}
+
+func IsValid(username string) bool {
+	return !strings.HasPrefix(username, "-") &&
+		!strings.HasSuffix(username, "-") &&
+		!strings.Contains(username, "--") &&
+		re.MatchString(username)
 }
