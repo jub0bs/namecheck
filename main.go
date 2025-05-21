fc1009f81e4694a9367f20911c097c23dda8c766
jub0bs <jcretel-infosec+github@protonmail.com>
2025-05-21 17:29:39 +0200

    exercise both packages in main

--- main.go
+++ main.go
@@ -2,14 +2,30 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/jub0bs/namecheck/bluesky"
 	"github.com/jub0bs/namecheck/github"
 )
 
 func main() {
 	username := "jub0bs"
-	if !github.IsValid(username) {
-		return
+	valid := github.IsValid(username)
+	fmt.Printf("validity of %q on GitHub: %t\n", username, valid)
+	if valid {
+		avail, err := github.IsAvailable(username)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("availability of %q on GitHub: %t\n", username, avail)
 	}
-	fmt.Println(username)
+	valid = bluesky.IsValid(username)
+	fmt.Printf("validity of %q on Bluesky: %t\n", username, valid)
+	if valid {
+		avail, err := bluesky.IsAvailable(username)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("availability of %q on Bluesky: %t\n", username, avail)
+	}
 }
