840f282531c92b802f53fa78b88cc5aaee622908
jub0bs <jcretel-infosec+github@protonmail.com>
2025-05-21 14:24:23 +0200

    move validation logic to IsValid function

--- main.go
+++ main.go
@@ -9,17 +9,21 @@ import (
 
 func main() {
 	username := "jub0bs"
+	if !IsValid(username) {
+		return
+	}
+	fmt.Println(username)
+}
+
+func IsValid(username string) bool {
 	if strings.HasPrefix(username, "-") ||
 		strings.HasSuffix(username, "-") ||
 		strings.Contains(username, "--") {
-		return
+		return false
 	}
 	valid, err := regexp.MatchString("^[A-Z0-9a-z-]{3,39}$", username)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if !valid {
-		return
-	}
-	fmt.Println(username)
+	return valid
 }
