9b1f5a763a9a6beb77219c665595af6355935037
jub0bs <jcretel-infosec+github@protonmail.com>
2025-05-21 12:11:53 +0200

    validate GitHub usernames

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	username := "jub0bs"
+	if strings.HasPrefix(username, "-") ||
+		strings.HasSuffix(username, "-") ||
+		strings.Contains(username, "--") {
+		return
+	}
+	valid, err := regexp.MatchString("^[A-Z0-9a-z-]{3,39}$", username)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !valid {
+		return
+	}
+	fmt.Println(username)
 }
