82fd977921a4ba81136656eb078c64ae5d268337
jub0bs <jcretel-infosec+github@protonmail.com>
2025-05-22 16:00:28 +0200

    use new Checker interface as common abstraction

--- main.go
+++ main.go
@@ -10,32 +10,30 @@ import (
 	"github.com/jub0bs/namecheck/github"
 )
 
+type Checker interface {
+	IsValid(string) bool
+	IsAvailable(string) (bool, error)
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "usage: %s <username>\n", os.Args[0])
 		os.Exit(1)
 	}
 	username := os.Args[1]
-	gh := github.GitHub{
-		Client: http.DefaultClient,
+	checkers := []Checker{
+		&github.GitHub{Client: http.DefaultClient},
+		&bluesky.Bluesky{},
 	}
-	valid := gh.IsValid(username)
-	fmt.Printf("validity of %q on GitHub: %t\n", username, valid)
-	if valid {
-		avail, err := gh.IsAvailable(username)
-		if err != nil {
-			log.Fatal(err)
+	for _, checker := range checkers {
+		valid := checker.IsValid(username)
+		fmt.Printf("validity of %q on GitHub: %t\n", username, valid)
+		if valid {
+			avail, err := checker.IsAvailable(username)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("availability of %q on GitHub: %t\n", username, avail)
 		}
-		fmt.Printf("availability of %q on GitHub: %t\n", username, avail)
-	}
-	var bs bluesky.Bluesky
-	valid = bs.IsValid(username)
-	fmt.Printf("validity of %q on Bluesky: %t\n", username, valid)
-	if valid {
-		avail, err := bs.IsAvailable(username)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("availability of %q on Bluesky: %t\n", username, avail)
 	}
 }
