be49d248f853d80b1153a83c191fbaa7b98b9abf
jub0bs <jcretel-infosec+github@protonmail.com>
2025-05-22 11:23:07 +0200

    store test cases in a mao instead of a slice

--- github/github_test.go
+++ github/github_test.go
@@ -9,21 +9,20 @@ import (
 
 func TestIsValid(t *testing.T) {
 	type TestCase struct {
-		desc     string
 		username string
 		want     bool
 	}
-	testCases := []TestCase{
-		{"contains two consecutive hyphens", "jub0bs--on-GitHub", false},
-		{"too short", "ab", false},
-		{"too long", strings.Repeat("a", 40), false},
-		{"starts with a hyphen", "-jub0bs", false},
-		{"ends with a hyphen", "jub0bs-", false},
-		{"contains illegal chars", "jub&obs", false},
-		{"all good", "jub0bs", true},
+	testCases := map[string]TestCase{
+		"contains two consecutive hyphens": {"jub0bs--on-GitHub", false},
+		"too short":                        {"ab", false},
+		"too long":                         {strings.Repeat("a", 40), false},
+		"starts with a hyphen":             {"-jub0bs", false},
+		"ends with a hyphen":               {"jub0bs-", false},
+		"contains illegal chars":           {"jub&obs", false},
+		"all good":                         {"jub0bs", true},
 		// other test cases...
 	}
-	for _, tc := range testCases {
+	for desc, tc := range testCases {
 		f := func(t *testing.T) {
 			got := github.IsValid(tc.username)
 			if got != tc.want {
@@ -31,6 +30,6 @@ func TestIsValid(t *testing.T) {
 				t.Errorf(tmpl, tc.username, got, tc.want)
 			}
 		}
-		t.Run(tc.desc, f)
+		t.Run(desc, f)
 	}
 }
