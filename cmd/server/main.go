a8383af021b2fae22747482d9cafc9c8b4052502
jub0bs <jcretel-infosec+github@protonmail.com>
2025-05-23 14:39:16 +0200

    adapt CLI tool to server

---
+++ cmd/server/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+
+	"github.com/jub0bs/namecheck/bluesky"
+	"github.com/jub0bs/namecheck/github"
+)
+
+type Checker interface {
+	IsValid(string) bool
+	IsAvailable(string) (bool, error)
+	fmt.Stringer
+}
+
+type Result struct {
+	Platform  string
+	Valid     bool
+	Available bool
+	Err       error
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /check", handleCheck)
+	if err := http.ListenAndServe(":8080", mux); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}
+
+func handleCheck(w http.ResponseWriter, r *http.Request) {
+	username := r.URL.Query().Get("username")
+	if username == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	gh := github.GitHub{Client: http.DefaultClient}
+	bs := bluesky.Bluesky{}
+	var checkers []Checker
+	for range 20 {
+		checkers = append(checkers, &gh, &bs)
+	}
+	resultCh := make(chan Result)
+	var wg sync.WaitGroup
+	for _, checker := range checkers {
+		wg.Add(1)
+		go check(checker, username, &wg, resultCh)
+	}
+	go func() {
+		wg.Wait()
+		close(resultCh)
+	}()
+	var results []Result
+	for res := range resultCh {
+		results = append(results, res)
+	}
+	fmt.Fprint(w, results)
+}
+
+func check(
+	checker Checker,
+	username string,
+	wg *sync.WaitGroup,
+	resultCh chan<- Result,
+) {
+	defer wg.Done()
+	res := Result{
+		Platform: checker.String(),
+		Valid:    checker.IsValid(username),
+	}
+	if !res.Valid {
+		resultCh <- res
+		return
+	}
+	res.Available, res.Err = checker.IsAvailable(username)
+	resultCh <- res
+}
