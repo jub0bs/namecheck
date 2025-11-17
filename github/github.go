d5e462dc92112d4655ad7838252aa586a51463d4
jub0bs <jcretel-infosec+github@protonmail.com>
2025-11-17 16:42:30 +0100

    implement availability check

--- github/github.go
+++ github/github.go
@@ -1,6 +1,8 @@
 package github
 
 import (
+	"fmt"
+	"net/http"
 	"regexp"
 	"strings"
 )
@@ -13,3 +15,19 @@ func IsValid(username string) bool {
 }
 
 var re = regexp.MustCompile("^[a-zA-Z0-9-]{3,39}$")
+
+func IsAvailable(username string) (bool, error) {
+	resp, err := http.Get("https://github.com/" + username)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+	switch resp.StatusCode {
+	case http.StatusOK:
+		return false, nil
+	case http.StatusNotFound:
+		return true, nil
+	default:
+		return false, fmt.Errorf("github: couldn't check availability of %q", username)
+	}
+}
