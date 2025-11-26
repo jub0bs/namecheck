c4f0492eecdb9cb23924d4afb55bc21da6e2deb3
jub0bs <jcretel-infosec+github@protonmail.com>
2025-11-26 15:18:16 +0100

    Revert "dedup usernames to test with a map"
    This reverts commit 0da782237a7aa4b17bbd10e79ac2dfc6b7e20b95.

--- main.go
+++ main.go
@@ -13,30 +13,25 @@ func main() {
 		fmt.Fprintf(os.Stderr, "usage: %s <username>\n", os.Args[0])
 		os.Exit(2)
 	}
-	usernames := make(map[string]struct{})
-	for _, name := range os.Args[1:] {
-		usernames[name] = struct{}{}
-	}
-	for username := range usernames {
-		valid := github.IsValid(username)
-		fmt.Printf("validity of %q on GitHub: %t\n", username, valid)
-		if valid {
-			avail, err := github.IsAvailable(username)
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				fmt.Printf("availability of %q on GitHub: %t\n", username, avail)
-			}
+	username := os.Args[1]
+	valid := github.IsValid(username)
+	fmt.Printf("validity of %q on GitHub: %t\n", username, valid)
+	if valid {
+		avail, err := github.IsAvailable(username)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("availability of %q on GitHub: %t\n", username, avail)
 		}
-		valid = bluesky.IsValid(username)
-		fmt.Printf("validity of %q on Bluesky: %t\n", username, valid)
-		if valid {
-			avail, err := bluesky.IsAvailable(username)
-			if err != nil {
-				fmt.Println(err)
-			} else {
-				fmt.Printf("availability of %q on Bluesky: %t\n", username, avail)
-			}
+	}
+	valid = bluesky.IsValid(username)
+	fmt.Printf("validity of %q on Bluesky: %t\n", username, valid)
+	if valid {
+		avail, err := bluesky.IsAvailable(username)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Printf("availability of %q on Bluesky: %t\n", username, avail)
 		}
 	}
 }
