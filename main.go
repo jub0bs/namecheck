4ed92a5e22ddfca52c1e828d7df10784f8e7b08e
jub0bs <jcretel-infosec+github@protonmail.com>
2026-09-09 15:42:32 +0200

    improve error handling and move stuff to github package

--- main.go
+++ main.go
@@ -2,32 +2,17 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"regexp"
 	"strings"
+
+	"github.com/jub0bs/namecheck/github"
 )
 
-const looksValid = "^[A-Za-z0-9-]{3,39}$"
-
 func main() {
-	fmt.Println(IsValid("jub0bs"))
-	fmt.Println(IsValid("jub0bs-"))
-	fmt.Println(IsValid("-jub0bs"))
-	fmt.Println(IsValid("jub--0bs"))
-	fmt.Println(IsValid("ju"))
-	fmt.Println(IsValid(strings.Repeat("a", 40)))
-	fmt.Println(IsValid("jub*bs"))
-}
-
-func IsValid(username string) bool {
-	if strings.HasPrefix(username, "-") ||
-		strings.Contains(username, "--") ||
-		strings.HasSuffix(username, "-") {
-		return false
-	}
-	match, err := regexp.MatchString(looksValid, username)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return match
+	fmt.Println(github.IsValid("jub0bs"))
+	fmt.Println(github.IsValid("jub0bs-"))
+	fmt.Println(github.IsValid("-jub0bs"))
+	fmt.Println(github.IsValid("jub--0bs"))
+	fmt.Println(github.IsValid("ju"))
+	fmt.Println(github.IsValid(strings.Repeat("a", 40)))
+	fmt.Println(github.IsValid("jub*bs"))
 }
