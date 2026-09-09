5004926b76faaa510e2d11c2d33b58e48ead9675
jub0bs <jcretel-infosec+github@protonmail.com>
2026-09-09 17:35:15 +0200

    flesh out main function

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/jub0bs/namecheck/bluesky"
 	"github.com/jub0bs/namecheck/github"
 )
 
@@ -20,4 +21,15 @@ func main() {
 		return
 	}
 	fmt.Printf("%q is valid and available on GitHub\n", username)
+	if !bluesky.IsValid(username) {
+		return
+	}
+	avail, err = bluesky.IsAvailable(username)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !avail {
+		return
+	}
+	fmt.Printf("%q is valid and available on Bluesky\n", username)
 }
