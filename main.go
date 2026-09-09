01ddbd64402f6167bed44215a3140ca9f2589633
jub0bs <jcretel-infosec+github@protonmail.com>
2026-09-09 16:23:53 +0200

    turn 'tests' into actual unit tests for the github package

--- main.go
+++ main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/jub0bs/namecheck/github"
 )
 
 func main() {
 	fmt.Println(github.IsValid("jub0bs"))
-	fmt.Println(github.IsValid("jub0bs-"))
-	fmt.Println(github.IsValid("-jub0bs"))
-	fmt.Println(github.IsValid("jub--0bs"))
-	fmt.Println(github.IsValid("ju"))
-	fmt.Println(github.IsValid(strings.Repeat("a", 40)))
-	fmt.Println(github.IsValid("jub*bs"))
 }
