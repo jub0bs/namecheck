8b2537717d040a0c29150fedaef12475248049d1
jub0bs <jcretel-infosec+github@protonmail.com>
2026-09-10 12:07:17 +0200

    Revert "accept multiple usernames to check"
    This reverts commit 8e21d1e4e0ac716f873f5d7e229e87383cbffa26.

--- main.go
+++ main.go
@@ -11,35 +11,30 @@ import (
 
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Fprint(os.Stderr, "usage: namecheck <username> [more-usernames]\n")
+		fmt.Fprint(os.Stderr, "usage: namecheck <username>\n")
 		os.Exit(1)
 	}
-	usernames := make(map[string]bool)
-	for _, name := range os.Args[1:] {
-		usernames[name] = true
+	username := os.Args[1]
+	if !github.IsValid(username) {
+		return
 	}
-	for username := range usernames {
-		if !github.IsValid(username) {
-			continue
-		}
-		avail, err := github.IsAvailable(username)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if !avail {
-			continue
-		}
-		fmt.Printf("%q is valid and available on GitHub\n", username)
-		if !bluesky.IsValid(username) {
-			continue
-		}
-		avail, err = bluesky.IsAvailable(username)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if !avail {
-			continue
-		}
-		fmt.Printf("%q is valid and available on Bluesky\n", username)
+	avail, err := github.IsAvailable(username)
+	if err != nil {
+		log.Fatal(err)
 	}
+	if !avail {
+		return
+	}
+	fmt.Printf("%q is valid and available on GitHub\n", username)
+	if !bluesky.IsValid(username) {
+		return
+	}
+	avail, err = bluesky.IsAvailable(username)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if !avail {
+		return
+	}
+	fmt.Printf("%q is valid and available on Bluesky\n", username)
 }
