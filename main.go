a40d7de518d229428ca95c6c7f7650b29bed2162
jub0bs <jcretel-infosec+github@protonmail.com>
2026-09-10 10:26:57 +0200

    get username as command-line argument

--- main.go
+++ main.go
@@ -3,13 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/jub0bs/namecheck/bluesky"
 	"github.com/jub0bs/namecheck/github"
 )
 
 func main() {
-	username := "jub0bs"
+	if len(os.Args) < 2 {
+		fmt.Fprint(os.Stderr, "usage: namecheck <username>\n")
+		os.Exit(1)
+	}
+	username := os.Args[1]
 	if !github.IsValid(username) {
 		return
 	}
